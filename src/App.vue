720586a01889606c503311d37bd211f17b96af4d
Nabokova Olga <olga.nabokova@fojin.tech>
2022-06-14 10:27:26 +0300

    create timer

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <TimerVue/>
+  <router-view />
 </template>
 
 <script>
-import TimerVue from '@/components/TimerVue.vue'
+  import { createRouter, createWebHistory } from 'vue-router'
 
-export default {
-  name: 'App',
-  components: {
-    TimerVue 
+  export default {
+    name: 'App'
   }
-}
-</script>
+
+  export const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path:'/', component: () => import('./pages/MainPage.vue') },
+      {
+        path:'/timer/:timerCategory',
+        name: 'timer',
+        component: () => import('./pages/TimerPage.vue'),
+        props: true
+      }
+    ], 
+  })
+
+</script> 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
+  *, ::before, ::after {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
 </style>
